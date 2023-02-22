4bfe339ee7546a60ad62c2889a4c893637009f6b
guanjiecheng <gcheng014@gmail.com>
2023-02-21 19:09:58 -0500

    startHW5

---
+++ Solution-hw-5/hw5-js/product.js
@@ -0,0 +1,57 @@
+//Hard codes values for glazing
+let allGlazes = [
+    {
+        choice: 'Keep original',
+        price: 0
+    },
+    {
+        choice: 'Sugar milk',
+        price: 0
+    },
+    {
+        choice: 'Vanilla milk',
+        price: 0.5
+    },
+    {
+        choice: 'Double chocolate',
+        price: 1.5
+    }
+]
+//hardcodes values for pack size
+let packSize = [
+    {
+        size: '1',
+        multiplier: 1
+    },
+    {
+        size: '3',
+        multiplier: 3
+    },
+    {
+        size: '6',
+        multiplier: 5
+    },
+    {
+        size: '12',
+        multiplier: 10
+    }
+]
+
+//selects the glazing select html and adds all options
+let glazingSelect = document.querySelector('#glazing')
+for(let i = 0; i <allGlazes.length; i++){
+    var option = document.createElement('option')
+    option.text = allGlazes[i].choice
+    option.value = i
+    glazingSelect.add(option)
+}
+
+//selects the size select html and adds all options
+let sizeSelect = document.querySelector('#size')
+for(let i = 0; i <packSize.length; i++){
+    var option = document.createElement('option')
+    option.text = packSize[i].size
+    option.value = i
+    sizeSelect.add(option)
+}
+
